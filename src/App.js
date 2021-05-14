9d9e48a24007ec9b3dd42e8803cc6f1c99a7d340
Ioannis Chrysostomakis <ic768@protonmail.com>
2021-05-14 17:25:35 +0300

    Api calls implemented, styling pending

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React,{useState,useEffect} from "react"
-import {getWeather} from "./api_calls"
+import {getLocations} from "./api_calls"
+import {Route,Link} from "react-router-dom"
+import WeatherView from "./WeatherView"
 import './App.css';
 
 function App() {
-	
 	const [filterQuery,setFilterQuery]=useState("")
+	const [locations,setLocations]=useState([])
 
 	useEffect(()=>{
-		(async()=>{ //
-			filterQuery && console.log(await getWeather(`/api/location/search/?query=${filterQuery}`)) //full address is proxied in package.json
+		(async()=>{ //filtering function
+			filterQuery.trim() && setLocations(await getLocations(filterQuery)) 
 		})()
-
 	},[filterQuery])
 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-				Test data
-				<input placeholder="Search by location" onChange={(e)=>{setFilterQuery(e.target.value)}}/>
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="Banner">
+				<input className="searchBar" placeholder="Search by location" onChange={(e) => {setFilterQuery(e.target.value)}} />
+			</div>
+			<div className="pageContainer">
+				<Route exact path="/">
+					{locations && locations.length > 0 && // many locations
+						<div className="LocationList">
+							{locations.map((location, i) =>
+								<Link key={location.woeid} to={`/${location.woeid}`}>
+									<h1 className="listItem--selectable" key={`${location.title}${i}`} onClick={() => {setLocations([location])}}>
+										{location.title}
+									</h1>
+								</Link>
+							)}
+						</div>
+					}
+				</Route>
+				<Route path="/:id">
+					<WeatherView region={locations[0]} />
+				</Route>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/api_calls.js
+++ src/api_calls.js
@@ -1,6 +1,11 @@
 import axios from "axios"
 
-export const getWeather = async (query) => {
-	const response = await axios.get(`${query}`)
+export const getLocations = async (query) => {
+	const response = await axios.get(`/search/?query=${query}`)
 	return response.data
+}
+
+export const getWeather = async (id) => {
+	const response = await axios.get(`/${id}/`)  //! taking away that trailing slash causes a CORS
+	return response.data.consolidated_weather
 }
